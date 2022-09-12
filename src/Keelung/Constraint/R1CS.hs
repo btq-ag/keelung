6905f1faef4c13d12cbab5987ff5f587b899889f
Luā Tîng-Giān <banacorn@gmail.com>
2022-09-12 05:15:33 +0000

    [ change ] Display of R1CS

--- src/Keelung/Constraint/R1CS.hs
+++ src/Keelung/Constraint/R1CS.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Keelung.Constraint.R1CS where
 
+import Control.DeepSeq (NFData)
 import Data.IntSet (IntSet)
 import qualified Data.IntSet as IntSet
 import Data.Serialize (Serialize)
@@ -11,7 +12,8 @@ import GHC.Generics (Generic)
 import qualified Keelung.Constraint.Polynomial as Poly
 import Keelung.Constraint.R1C (R1C (..))
 import Keelung.Types (Var)
-import Control.DeepSeq (NFData)
+import Keelung.Field (N(..))
+import qualified Data.List as List
 
 --------------------------------------------------------------------------------
 
@@ -34,37 +36,54 @@ data R1CS n = R1CS
 
 instance Serialize n => Serialize (R1CS n)
 
-instance (Show n, Integral n) => Show (R1CS n) where
-  show r1cs@(R1CS cs n is _ os _) =
+instance (Show n, Integral n, Bounded n, Fractional n) => Show (R1CS n) where
+  show r1cs@(R1CS cs n is bis os _) =
     "R1CS {\n\
     \  R1C constraints ("
-      <> show numberOfConstraints
+      <> show (length cs + IntSet.size bis)
       <> "):\n\n"
       <> showConstraints
-      ++ "\n  number of variables: "
+      ++ "\n  Number of variables: "
       ++ show n
-      ++ "\n  input  variables: "
+      <> showBooleanInputVarNumber
+      ++ "\n  Input  variables ("
+      <> show is
+      <> "): "
       ++ inputVars
-      ++ "\n  output variables: "
-      ++ show (IntSet.toList os)
+      ++ "\n  Output variables ("
+      <> show (IntSet.size os)
+      <> "): "
+      ++ outputVars
       ++ "\n}"
     where
+      -- prettify input variables
       inputVars = case is of
         0 -> "none"
-        1 -> "$0"
-        _ -> "$0 .. $" <> show (is - 1)
-      numberOfConstraints = length cs + is
+        1 -> "[ $0 ]"
+        _ -> "[ $0 .. $" <> show (is - 1) <> " ]"
+      -- prettify output variables
+      outputVars =
+        if IntSet.null os
+          then "none"
+          else "[ " <> List.intercalate ", " (map (\v -> "$" <> show v) (IntSet.toList os)) <> " ]"
+
       constraints = toR1Cs r1cs
-      showConstraints = if null constraints 
-        then "none"
-        else unlines (map (\s -> "    " ++ show s) constraints)
+      showConstraints =
+        if null constraints
+          then "none"
+          else unlines (map (\s -> "    " ++ show (fmap N s)) constraints)
+
+      showBooleanInputVarNumber =
+        if null constraints
+          then ""
+          else "\n  Number of Boolean input variables: " <> show (IntSet.size bis)
 
 -- | Return R1Cs from a R1CS
---   (includes boolean constraints for input variables)
+--   (includes constraints of boolean input variables)
 toR1Cs :: (Num n, Eq n) => R1CS n -> [R1C n]
 toR1Cs (R1CS cs _ _ bis _ _) = cs <> booleanInputVarConstraints
   where
-    booleanInputVarConstraints :: (Num n, Eq n) =>[R1C n]
+    booleanInputVarConstraints :: (Num n, Eq n) => [R1C n]
     booleanInputVarConstraints =
       map
         ( \var ->
