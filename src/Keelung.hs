629fa0717bab79be41560b973c16d022e0ca8b42
Ting-gian LUA <banacorn@gmail.com>
2023-09-20 16:19:16 +0800

    [ new ] Bump version to v0.16.0

--- src/Keelung.hs
+++ src/Keelung.hs
@@ -66,7 +66,7 @@ import Text.Read (readMaybe)
 
 -- | IMPORTANT: The compatibale compiler version of this library, Make sure it's updated and matched accordingly.
 keelungCompilerVersion :: (Int, Int)
-keelungCompilerVersion = (0, 15)
+keelungCompilerVersion = (0, 16)
 
 -- | Patch version of this library
 compilerPatchVersion :: Int
--- src/Keelung/Error.hs
+++ src/Keelung/Error.hs
@@ -33,7 +33,7 @@ instance Show Error where
   show CannotReadVersionError = "Cannot read the version of the Keelung compiler"
   show (VersionMismatchError major minor patch) =
     "The version of the Keelung compiler is not supported: \n"
-      ++ "  expected range of version: >= v0.15.0 and < v0.16.0, but got v"
+      ++ "  expected range of version: >= v0.16.0 and < v0.17.0, but got v"
       ++ show major
       ++ "."
       ++ show minor
