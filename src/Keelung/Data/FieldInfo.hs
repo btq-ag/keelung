4130d07017955e794d91e5c719fcf10ee591bd02
Ting-gian LUA <banacorn@gmail.com>
2024-04-10 14:04:02 +0800

    [ new ] FieldInfo.fromFieldType`

--- src/Keelung/Data/FieldInfo.hs
+++ src/Keelung/Data/FieldInfo.hs
@@ -1,18 +1,18 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Keelung.Data.FieldInfo (FieldInfo (..), caseFieldType) where
+module Keelung.Data.FieldInfo (FieldInfo (..), caseFieldType, fromFieldType) where
 
 import Control.DeepSeq (NFData)
 import Data.Field.Galois
 import Data.Proxy
+import Data.Serialize (Serialize)
 import GHC.Generics (Generic)
 import GHC.TypeLits
 import Keelung.Field (FieldType (..))
-import Data.Serialize (Serialize)
 
 -- | Runtime info data of a field
 data FieldInfo = FieldInfo
@@ -29,30 +29,40 @@ instance Serialize FieldInfo
 -- | Induction on `FieldInfo`
 caseFieldType ::
   FieldType ->
-  (forall n. KnownNat n => Proxy (Prime n) -> FieldInfo -> IO a) ->
-  (forall n. KnownNat n => Proxy (Binary n) -> FieldInfo -> IO a) ->
+  (forall n. (KnownNat n) => Proxy (Prime n) -> FieldInfo -> IO a) ->
+  (forall n. (KnownNat n) => Proxy (Binary n) -> FieldInfo -> IO a) ->
   IO a
 caseFieldType (Prime n) funcPrime _ = case someNatVal n of
-  Just (SomeNat (_ :: Proxy n)) -> do
-    let fieldNumber = asProxyTypeOf 0 (Proxy :: Proxy (Prime n))
-     in funcPrime (Proxy :: Proxy (Prime n)) $
-          FieldInfo
+  Just (SomeNat (_ :: Proxy n)) -> funcPrime (Proxy :: Proxy (Prime n)) (fromFieldType (Prime n))
+  Nothing -> error "[ panic ] caseFieldType: someNatVal failure"
+caseFieldType (Binary n) _ funcBinary = case someNatVal n of
+  Just (SomeNat (_ :: Proxy n)) -> funcBinary (Proxy :: Proxy (Binary n)) (fromFieldType (Binary n))
+  Nothing -> error "[ panic ] caseFieldType: someNatVal failure"
+
+-- | Converting from `FieldType`
+fromFieldType ::
+  FieldType ->
+  FieldInfo
+fromFieldType (Prime n) =
+  case someNatVal n of
+    Just (SomeNat (_ :: Proxy n)) -> do
+      let fieldNumber = asProxyTypeOf 0 (Proxy :: Proxy (Prime n))
+       in FieldInfo
             { fieldTypeData = Prime n,
               fieldOrder = toInteger (order fieldNumber),
               fieldChar = char fieldNumber,
               fieldDeg = fromIntegral (deg fieldNumber),
               fieldWidth = floor (logBase (2 :: Double) (fromIntegral (order fieldNumber)))
             }
-  Nothing -> error "[ panic ] caseFieldType: someNatVal failure"
-caseFieldType (Binary n) _ funcBinary = case someNatVal n of
+    Nothing -> error "[ panic ] toFieldInfo: someNatVal failure"
+fromFieldType (Binary n) = case someNatVal n of
   Just (SomeNat (_ :: Proxy n)) ->
     let fieldNumber = asProxyTypeOf 0 (Proxy :: Proxy (Binary n))
-     in funcBinary (Proxy :: Proxy (Binary n)) $
-          FieldInfo
-            { fieldTypeData = Binary n,
-              fieldOrder = toInteger (order fieldNumber),
-              fieldChar = char fieldNumber,
-              fieldDeg = fromIntegral (deg fieldNumber),
-              fieldWidth = floor (logBase (2 :: Double) (fromIntegral (order fieldNumber)))
-            }
-  Nothing -> error "[ panic ] caseFieldType: someNatVal failure"
+     in FieldInfo
+          { fieldTypeData = Binary n,
+            fieldOrder = toInteger (order fieldNumber),
+            fieldChar = char fieldNumber,
+            fieldDeg = fromIntegral (deg fieldNumber),
+            fieldWidth = floor (logBase (2 :: Double) (fromIntegral (order fieldNumber)))
+          }
+  Nothing -> error "[ panic ] toFieldInfo: someNatVal failure"
